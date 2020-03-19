620da8122b32e28513ed22cc2e0f7bc87e8e06a7
Victor PULBERE <vpulbere@bitdefender.com>
2020-03-19 21:29:11 +0200

    First commit

---
+++ src/main/java/com/example/lab1/Product.java
@@ -0,0 +1,30 @@
+package com.example.lab1;
+
+public class Product {
+    private String name;
+    private String description;
+    private Double price;
+
+    public Product(String name, String description, Double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDescription() + ". \nPrice: " + this.getPrice() + "RON";
+    }
+}
